eafe4bedc4027be497ca2421d0d05377a703a59d
Prometium <akrasnovskii@yandex.ru>
2021-03-28 23:52:08 +0300

    implement drag and resize click area

--- editor-web-app/src/components/Frame.vue
+++ editor-web-app/src/components/Frame.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="frame">
-    <div ref="resizeDrag" class="resize-drag">
+    <div class="frame__img-wrapper">
       <img
+        ref="img"
         :src="frame.pictureLink + '#' + new Date().getTime()"
         :alt="frame.uid"
         class="frame__img"
       />
+      <div ref="resizeDrag" class="resize-drag" />
     </div>
   </div>
 </template>
@@ -17,43 +19,122 @@ import interact from "interactjs";
 
 export default Vue.extend({
   name: "Frame",
-  computed: {
-    ...mapState(["frame"])
+  data() {
+    return {
+      scale: 1
+    };
   },
   mounted() {
-    let resizeDrag = this.$refs.resizeDrag;
+    const resizeDrag = this.$refs.resizeDrag;
     this.initInteract(resizeDrag);
+
+    window.addEventListener("resize", this.onResize);
+  },
+  beforeDestroy() {
+    window.removeEventListener("resize", this.onResize);
+  },
+  computed: {
+    ...mapState(["frame", "script"]),
+    currentBranchNum() {
+      return this.script.branchNumByUid[this.frame.uid] || 0;
+    },
+    selectedAction() {
+      return this.frame.actions && this.frame.actions[this.currentBranchNum];
+    }
+  },
+  watch: {
+    selectedAction(action) {
+      this.onResize();
+      const img = this.$refs.img;
+      const scale = img.clientWidth / img.naturalWidth || 1;
+
+      const resizeDrag = this.$refs.resizeDrag;
+      resizeDrag.setAttribute("data-scale", scale);
+
+      resizeDrag.style.webkitTransform = resizeDrag.style.transform = `
+      translate(${action.xLeft * scale}px, ${action.yLeft * scale}px)
+      `;
+      resizeDrag.style.width = `${(action.xRight - action.xLeft) * scale}px`;
+      resizeDrag.style.height = `${(action.yRight - action.yLeft) * scale}px`;
+
+      resizeDrag.setAttribute("data-x", action.xLeft * scale);
+      resizeDrag.setAttribute("data-y", action.yLeft * scale);
+      resizeDrag.setAttribute("data-fixed-x", action.xLeft);
+      resizeDrag.setAttribute("data-fixed-y", action.yLeft);
+      resizeDrag.setAttribute("data-fixed-width", action.xRight - action.xLeft);
+      resizeDrag.setAttribute(
+        "data-fixed-height",
+        action.yRight - action.yLeft
+      );
+    }
   },
   methods: {
+    onResize() {
+      const img = this.$refs.img;
+      const scale = img.clientWidth / img.naturalWidth || 1;
+
+      const resizeDrag = this.$refs.resizeDrag;
+      resizeDrag.setAttribute("data-scale", scale);
+
+      const x = parseFloat(resizeDrag.getAttribute("data-fixed-x")) || 0;
+      const y = parseFloat(resizeDrag.getAttribute("data-fixed-y")) || 0;
+      const width = +resizeDrag.getAttribute("data-fixed-width") || 0;
+      const height = +resizeDrag.getAttribute("data-fixed-height") || 0;
+      console.log(width, width * scale);
+      resizeDrag.style.webkitTransform = resizeDrag.style.transform = `
+      translate(${x * scale}px, ${y * scale}px)
+      `;
+      resizeDrag.style.width = `${width * scale}px`;
+      resizeDrag.style.height = `${height * scale}px`;
+
+      resizeDrag.setAttribute("data-x", x * scale);
+      resizeDrag.setAttribute("data-y", y * scale);
+    },
     initInteract(selector) {
       interact(selector)
+        .draggable({
+          onend(event) {
+            const target = event.target;
+            const x = parseFloat(target.getAttribute("data-x")) || 0;
+            const y = parseFloat(target.getAttribute("data-y")) || 0;
+            const scale = parseFloat(target.getAttribute("data-scale")) || 1;
+
+            target.setAttribute("data-fixed-x", x / scale);
+            target.setAttribute("data-fixed-y", y / scale);
+          }
+        })
         .resizable({
           // resize from all edges and corners
           edges: { left: true, right: true, bottom: true, top: true },
 
           listeners: {
             move(event) {
-              var target = event.target;
-              var x = parseFloat(target.getAttribute("data-x")) || 0;
-              var y = parseFloat(target.getAttribute("data-y")) || 0;
+              const target = event.target;
+              let x = parseFloat(target.getAttribute("data-x")) || 0;
+              let y = parseFloat(target.getAttribute("data-y")) || 0;
+              const scale = parseFloat(target.getAttribute("data-scale")) || 1;
 
               // update the element's style
-              target.style.width = event.rect.width + "px";
-              target.style.height = event.rect.height + "px";
+              target.style.width = `${event.rect.width}px`;
+              target.style.height = `${event.rect.height}px`;
 
               // translate when resizing from top or left edges
               x += event.deltaRect.left;
               y += event.deltaRect.top;
 
-              target.style.webkitTransform = target.style.transform =
-                "translate(" + x + "px," + y + "px)";
+              target.style.webkitTransform = target.style.transform = `
+              translate(${x}px, ${y}px)
+              `;
 
               target.setAttribute("data-x", x);
               target.setAttribute("data-y", y);
-              target.textContent =
-                Math.round(event.rect.width) +
-                "\u00D7" +
-                Math.round(event.rect.height);
+              target.setAttribute("data-fixed-x", x / scale);
+              target.setAttribute("data-fixed-y", y / scale);
+              target.setAttribute("data-fixed-width", event.rect.width / scale);
+              target.setAttribute(
+                "data-fixed-height",
+                event.rect.height / scale
+              );
             }
           },
           modifiers: [
@@ -64,7 +145,7 @@ export default Vue.extend({
 
             // minimum size
             interact.modifiers.restrictSize({
-              min: { width: 100, height: 50 }
+              min: { width: 5, height: 5 }
             })
           ],
 
@@ -107,20 +188,30 @@ export default Vue.extend({
   overflow-y: auto;
 }
 
+.frame__img-wrapper {
+  position: relative;
+}
+
 .frame__img {
   max-height: 100%;
   max-width: 100%;
 }
 
+@keyframes shine {
+  from {
+    background-color: rgba(33, 33, 33, 70%);
+  }
+  to {
+    background-color: rgba(207, 216, 220, 70%);
+  }
+}
+
 .resize-drag {
-  width: 120px;
-  border-radius: 8px;
-  padding: 20px;
-  margin: 1rem;
-  background-color: #29e;
-  color: white;
-  font-size: 20px;
-  font-family: sans-serif;
+  position: absolute;
+  top: 0;
+  left: 0;
+  padding: 5px;
   touch-action: none;
+  animation: 4s ease-in-out infinite alternate shine;
 }
 </style>
